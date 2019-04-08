a1c22d3f25b15f518091b4e0832e9c820850b202
Alexis <alexis.morin.mail@gmail.com>
2019-04-08 10:12:38 -0400

    add gym and mouse AI script

---
+++ realtime-short/Assets/Scripts/MouseAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouseAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator rig;
+    public Transform[] waypoints;
+    public string[] additiveAnimationTriggerNames;
+    int index = -1;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    public void GoToNextWaypoint()
+    {   
+        index++;
+        agent.destination = waypoints[index].position;
+    }
+
+    public void PlayAdditiveAnimation(){
+        rig.SetTrigger("additiveAnimationTriggerNames");
+    }
+}
