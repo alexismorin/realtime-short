79eabc553272d197b526a009384aa46469c8158f
Alexis <alexis.morin.mail@gmail.com>
2019-04-10 11:38:26 -0400

    added lamp material

---
+++ realtime-short/Assets/Scripts/Force24fps.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force24fps : MonoBehaviour
+{
+
+public int targetFPS = 12;
+
+ void Awake () {
+
+     QualitySettings.vSyncCount = 0;  // VSync must be disabled
+     Application.targetFrameRate = targetFPS;
+
+ }
+}
